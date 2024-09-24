2ffd3f0f4d7c3283e562492490bb29c069b2889a
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-09-24 12:52:45 +0200

    no message

--- ejercicios.js
+++ ejercicios.js
@@ -1,9 +1,8 @@
 //1 
 let tb = "The Bridge 2023";
-let tb2 = tb.split(" ");
 
 console.log(tb.length);
-console.log(tb2);
+console.log(tb.substring(0, 10));
 
 
 //2
@@ -27,17 +26,19 @@ let array = [1,2,3,4,5,6,7,8,9,10];
 
 for (let i = 0; i < array.length; i++) {
     if(array[i] % 2 == 0) {
-        return console.log(array[i]);
+        console.log(array[i]);
     }
 }
 
 //6 
 let p = document.getElementById("miParrafo");
 let b = document.getElementById("miBoton");
+let l = document.getElementById("miLista");
 
-p.textContent("Nuevo Texto");
+p.innerText = "Nuevo Texto";
 p.style.color="red";
 p.style.fontWeight="bold";
-document.getElementById("miLista").innerHTML("<li>Nuevo elemento de lista</li>");
-miBoton.addEventListener('click', clickar);
-function clickar() {alert("Has hecho click")};
+l.innerHTML = "<li>Nuevo elemento de lista</li>";
+b.addEventListener('click', function(event){
+    alert("Has hecho click");
+ });
