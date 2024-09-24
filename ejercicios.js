03761d8dc82bde5da60cf6a283d1b1d4005d3e1d
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-09-24 10:13:49 +0200

    no message

---
+++ ejercicios.js
@@ -0,0 +1,30 @@
+//1 usar el forEach
+let tb = "The Bridge 2023";
+
+
+//2
+let culpable;
+
+if (culpable == true) {
+    console.log("Es culpable");
+} else {
+    console.log("No es culpable");
+}
+
+//3 pasar el argumento y saber tipo de la variable
+let nombre;
+
+function saludar (nombre) {
+    return console.log("Hola, soy " + nombre);
+}
+
+//5
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < array.length; i++) {
+    if(array[i] % 2 == 0) {
+        return console.log(array[i]);
+    }
+}
+
+//6 modificar contenido html
